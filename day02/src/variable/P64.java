683b75499a3cd1228166f587f73d3caa84052dea
iiinbo <124110271+iiinbo@users.noreply.github.com>
2023-02-23 16:25:13 +0900

    Update P64.java
    2023 update

--- day02/src/variable/P64.java
+++ day02/src/variable/P64.java
@@ -4,7 +4,7 @@ public class P64 {
 
 	public static void main(String[] args) {
 		int a = 10;
-		double d = 10.23456;
+		int d = 10;
 		String str1 = "abc";
 		String str2 = "abc";
 		str1 = "def"; //abc값은 메모리에 남아있고, str1의 주소도 바뀌고 def가 새로 만들어진다.
