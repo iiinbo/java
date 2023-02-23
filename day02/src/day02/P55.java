3f6741c07142ded921c00b325f0f992f4838eb27
iiinbo <bo2939@naver.com>
2023-02-23 09:57:37 +0900

    2023 02 22 과제

---
+++ day02/src/day02/P55.java
@@ -0,0 +1,14 @@
+package day02;
+
+public class P55 {
+
+	public static void main(String[] args) {
+		int a = 2000000000;
+		int b = 2000000000;
+		
+		long l = a;
+		long t = b;
+		System.out.println(l + t);
+	}
+
+}
